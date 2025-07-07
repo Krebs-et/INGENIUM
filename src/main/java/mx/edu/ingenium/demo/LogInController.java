68cdaf960d81098170880b7af20a12a58f35dcf1
EDUARDOlux <20243ds106@utez.edu.mx>
2025-07-07 15:29:18 -0600

    cambios lalo

--- src/main/java/mx/edu/ingenium/demo/LogInController.java
+++ src/main/java/mx/edu/ingenium/demo/LogInController.java
@@ -1,4 +1,43 @@
 package mx.edu.ingenium.demo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class LogInController {
+
+@FXML
+public Button IniciarSesion;
+
+    @FXML
+    private void cerrarSesion(ActionEvent event) {
+        //Codigo de Funcion segun LUIS
+        try {
+            //1. necesitamos cargar mi nueva ventana
+            Parent login = FXMLLoader.load(getClass().getResource("index.fxml")
+            );
+
+            //2 Obtener el stage que ya existia previamente
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //3. Definir oh preparar la nueva ecena
+            Scene escena = new Scene(login);
+            //4 =. Poner la nueva ecena
+            stage.setScene(escena);
+            //5. asegurarno de que de vea
+            stage.show();
+
+
+        }catch(Exception e){
+            System.out.println("Ocurrio un erro");
+
+        }
+    }
+
+
 }
--- src/main/java/mx/edu/ingenium/demo/RegisterController.java
+++ src/main/java/mx/edu/ingenium/demo/RegisterController.java
@@ -1,4 +1,5 @@
 package mx.edu.ingenium.demo;
 
 public class RegisterController {
+
 }
